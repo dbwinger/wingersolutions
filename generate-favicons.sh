cce518eca137bfe533af50488926a979c51bf3c8
dbwinger <dbwinger@gmail.com>
2025-07-02 16:13:51 -0400

    Add favicon with WS logo in Hacker theme colors

---
+++ generate-favicons.sh
@@ -0,0 +1,10 @@
+# Generate PNG favicons from SVG
+# You can run this if you have ImageMagick installed
+# convert assets/favicon.svg -resize 32x32 assets/favicon-32x32.png
+# convert assets/favicon.svg -resize 16x16 assets/favicon-16x16.png
+
+# Or use online converters:
+# 1. Go to favicon.io/favicon-converter
+# 2. Upload your favicon.svg
+# 3. Download the generated files
+# 4. Add favicon-32x32.png and favicon-16x16.png to assets/ folder
